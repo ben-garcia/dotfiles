dcee09799905a31fbe63d65f8c9551d9ea9c6af1
= <7rubengarcia7@gmail.com>
2018-11-21 10:09:01 -0800

    basic files

---
+++ config.fish
@@ -0,0 +1,3 @@
+# set PATH variable
+set -x PATH /home/ben/.yarn/bin /home/ben/.nvm/versions/node/v11.0.0/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /snap/bin
+
