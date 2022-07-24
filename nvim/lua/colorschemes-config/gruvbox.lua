c85e2efee6f7f582e84c4ad0aaa3a69b25b7f0d5
ben <7rubengarcia7@gmail.com>
2022-07-24 10:15:06 -0700

    chore(nvim): set 'italic' to false

--- nvim/lua/colorschemes-config/gruvbox.lua
+++ nvim/lua/colorschemes-config/gruvbox.lua
@@ -1,2 +1,18 @@
-vim.opt.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+-- setup must be called before loading the colorscheme
+-- Default options:
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = false,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  overrides = {},
+})
+
+vim.cmd("colorscheme gruvbox")
